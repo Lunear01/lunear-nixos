525db2b999f1bde3b13d9509ee05c2d8ec87ed22
Lunear01 <Ikerhuang0315@gmail.com>
2026-06-18 14:15:32 -0400

    Changed sys d entry to show recent 3 generations

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "lunear-nixos";
